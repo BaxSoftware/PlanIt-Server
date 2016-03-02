ee566f56ea31cb19393a5a4ad96327a5bec5847e
Farzad Daei <farzaddaei@gmail.com>
2016-03-01 18:49:46 -0800

    Initialized project with Mobile App Azure Service

---
+++ PlanIt-Server/PlanIt-Server/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PlanIt_Server.Startup))]
+
+namespace PlanIt_Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
